a909cbaa35f8d11aa148a2d86dd3ea1e59f06ba7
mark-lenovo-win <2656233499@qq.com>
2022-09-19 10:11:54 +0800

    HWv2.0;anti back wash;grid bool matrix;code style

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -7,92 +7,120 @@ import java.util.ArrayList;
 public class Percolation {
     private static int scale;
     private WeightedQuickUnionUF grid;
-    private ArrayList<Integer> opened_site=new ArrayList();
+    private boolean[][] grid_matrix;
+    // crteate another grid to avoid back wash.
+    private WeightedQuickUnionUF grid_antibackwash;
     private int vir_top;
     private int vir_bottom;
+
+    private int openSite_num = 0;
+
     public Percolation(int N)                // create N-by-N grid, with all sites initially blocked
     {
         printAruException(N);
         scale = N;
         vir_top = scale * scale;
-        vir_bottom =vir_top + 1;
+        vir_bottom = vir_top + 1;
+        grid_matrix = new boolean[scale][scale];
 
-        grid = new WeightedQuickUnionUF(N*N + 2);
-        for(int i=0;i < N;i++){   //create virtual site
-            grid.union(i,vir_top);
-            grid.union(N*N-1-i,vir_bottom);
-        }
+        grid = new WeightedQuickUnionUF(N * N + 2);
+
+        grid_antibackwash = new WeightedQuickUnionUF(N * N + 1);
+
     }
+
     public void open(int row, int col)       // open the site (row, col) if it is not open already
     {
         // add Exception
-       printBouException(row);
-       printBouException(col);
+        printBouException(row);
+        printBouException(col);
 
-        if (isOpen(row,col)){
+
+        // show neighbor sites' position.
+        int next[][] = new int[][]{
+                {0, 1},
+                {1, 0},
+                {0, -1},
+                {-1, 0}
+        };
+
+        if (isOpen(row, col)) {
             return;
         }
 
-        for(int i = 0;i < opened_site.size();i++){
-            union(xyTo1d(row,col),opened_site.get(i));
+        grid_matrix[row][col] = true;
+        openSite_num += 1;
+
+
+        // union neighbor sites
+        for (int i = 0; i < 4; i++) {
+            int row_n = row + next[i][0];
+            int col_n = col + next[i][1];
+
+            if ((col_n < scale) && (col_n >= 0)) {
+                if (row_n == scale) {
+                    grid.union(xyTo1d(row, col), vir_bottom);
+                    continue;
+                } else if (row_n == -1) {
+                    grid.union(xyTo1d(row, col), vir_top);
+                    grid_antibackwash.union(xyTo1d(row, col), vir_top);
+                    continue;
+                } else {
+                    if (isOpen(row_n, col_n)) {
+                        grid.union(xyTo1d(row, col), xyTo1d(row_n, col_n));
+                        grid_antibackwash.union(xyTo1d(row, col), xyTo1d(row_n, col_n));
+                    }
+                }
+            }
         }
-        opened_site.add(xyTo1d(row,col));
+
 
     }
+
     public boolean isOpen(int row, int col)  // is the site (row, col) open?
     {
         printBouException(row);
         printBouException(col);
-        return opened_site.contains(xyTo1d(row,col));
+        return grid_matrix[row][col];
     }
+
     public boolean isFull(int row, int col)  // is the site (row, col) full?
     {
         printBouException(row);
         printBouException(col);
-        if(isOpen(row, col)){
-            return grid.connected(xyTo1d(row,col),vir_top);
+        if (isOpen(row, col)) {
+            return grid_antibackwash.connected(xyTo1d(row, col), vir_top);
         }
         return false;
     }
+
     public int numberOfOpenSites()           // number of open sites
     {
-        return opened_site.size();
+        return openSite_num;
     }
+
     public boolean percolates()              // does the system percolate?
     {
-        return grid.connected(vir_bottom,vir_top);
+        return grid.connected(vir_bottom, vir_top);
     }
-    private int xyTo1d(int x,int y){
+
+    private int xyTo1d(int x, int y) {
         return x * scale + y;
     }
 
-    private void union(int x,int y){
-        int a = x-y;
-        if(Math.abs(a)==1){
-            if((x/scale)==(y/scale)) {
-                grid.union(x, y);
-                return;
-            }
-        }
-        else if (Math.abs(a) == scale) {
-            grid.union(x,y);
-            return;
-        }
-        return;
-    }
 
-    private void printAruException(int x){
-        if(x<=0)
-        {
+    private void printAruException(int x) {
+        if (x <= 0) {
             throw new java.lang.IllegalArgumentException();
         }
     }
 
-    private void printBouException(int x){
-        if((x<0)||(x>(scale-1))){
+    private void printBouException(int x) {
+        if ((x < 0) || (x > (scale - 1))) {
             throw new java.lang.IndexOutOfBoundsException();
         }
     }
+
     public static void main(String[] args)   // use for unit testing (not required)
     {
 
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -8,9 +8,10 @@ public class PercolationStats {
     private double[] result;
     private double res_mean;
     private double res_stddev;
+
     public PercolationStats(int N, int T, PercolationFactory pf)   // perform T independent experiments on an N-by-N grid
     {
-        if((N<=0)||(T<=0)){
+        if ((N <= 0) || (T <= 0)) {
             throw new java.lang.IllegalArgumentException();
         }
 
@@ -18,18 +19,18 @@ public class PercolationStats {
         result = new double[T];
 
         // T times trials
-        for(int times = 0;times < trial;T++){
+        for (int times = 0; times < trial; T++) {
             StdRandom.setSeed(times);
             Percolation newgrid = pf.make(N);
-            while (newgrid.percolates()){
-                int row =StdRandom.uniform(0,N);
-                int col =StdRandom.uniform(0,N);
+            while (newgrid.percolates()) {
+                int row = StdRandom.uniform(0, N);
+                int col = StdRandom.uniform(0, N);
 
-                if(!newgrid.isOpen(row,col)){
-                    newgrid.open(row,col);
+                if (!newgrid.isOpen(row, col)) {
+                    newgrid.open(row, col);
                 }
             }
-            result[times] = ((double)newgrid.numberOfOpenSites())/(N*N);
+            result[times] = ((double) newgrid.numberOfOpenSites()) / (N * N);
         }
 
         res_mean = mean();
@@ -40,18 +41,22 @@ public class PercolationStats {
 
 
     }
+
     public double mean()                                           // sample mean of percolation threshold
     {
         return StdStats.mean(result);
     }
+
     public double stddev()                                         // sample standard deviation of percolation threshold
     {
         return StdStats.stddev(result);
     }
-    public double confidenceLow()                                  // low endpoint of 95% confidence interval
+
+    public double confidenceLow()                                  // low endpoint of 96% confidence interval
     {
         return res_mean - (1.96 * res_stddev) / (Math.sqrt(trial));
     }
+
     public double confidenceHigh()                                 // high endpoint of 95% confidence interval
     {
         return res_mean + (1.96 * res_stddev) / (Math.sqrt(trial));
